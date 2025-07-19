c8d4650974f18bdd6f6f87dcb92c6a6873514c5a
Ro <rociozentrigen@gmail.com>
2025-07-19 19:19:26 -0300

    Cambio en mensaje de validacion de firstName y lastName

--- types/user.ts
+++ types/user.ts
@@ -6,8 +6,8 @@ export type UserData = z.infer<typeof NewUser>
 
 export const UserFormSchema = UserSchema.omit({ name: true, id: true, role: true }).extend(
   {
-    firstName: z.string().min(1, "El nombre es obligatorio"),
-    lastName: z.string().min(1, "El apellido es obligatorio"),
+    firstName: z.string().min(1, "Por favor, proporcione un nombre"),
+    lastName: z.string().min(1, "Por favor, proporcione un apellido"),
     password: z.string().min(6, "La contraseña debe tener al menos 6 caracteres"),
     confirmPassword: z.string().min(6, "La confirmación de contraseña debe tener al menos 6 caracteres"),
     acceptTerms: z.boolean(),
