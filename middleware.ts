1d338aeee78bd0da8c149db990d7dfadf87428e2
rocio <rociozentrigen@gmail.com>
2026-02-25 20:48:01 -0300

    Fix middleware attempt (previous version)

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,8 @@
 import NextAuth from "next-auth";
-import { authConfig } from "./auth.config";
+import { authConfig } from "@/auth.config";
 
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  runtime: 'nodejs',
   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
